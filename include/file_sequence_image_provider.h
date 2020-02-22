2263c56e19630ce0678880faa325ad7d499d6bed
Alvaro <alvgaona@gmail.com>
2020-02-22 00:38:59 -0300

    feat: Fetch image from a sequence of images

--- include/file_sequence_image_provider.h
+++ include/file_sequence_image_provider.h
@@ -1,22 +1,27 @@
-#ifndef FILE_SEQUENCE_IMAGE_PROVIDER_H
-#define FILE_SEQUENCE_IMAGE_PROVIDER_H
-
-#include "image_provider.h"
+#ifndef EKF_MONO_SLAM_FILE_SEQUENCE_IMAGE_PROVIDER_H_
+#define EKF_MONO_SLAM_FILE_SEQUENCE_IMAGE_PROVIDER_H_
 
 #include <exception>
-#include <string>
 #include <filesystem>
 #include <iostream>
+#include <string>
+
+#include "image_file_type.h"
+#include "image_file_utils.h"
+#include "image_provider.h"
 
 class FileSequenceImageProvider : public ImageProvider {
  public:
   FileSequenceImageProvider(std::string directory);
-  ~FileSequenceImageProvider();
-  virtual cv::Mat& GetNextImage() override;
+  virtual ~FileSequenceImageProvider() = default;
+  virtual cv::Mat GetNextImage() override;
 
  private:
-  std::filesystem::directory_iterator it;
+  std::filesystem::path directory;
+  int start_index_;
+  int end_index_;
   int image_counter_;
+  ImageFileType image_type_;
 };
 
-#endif
+#endif /* EKF_MONO_SLAM_FILE_SEQUENCE_IMAGE_PROVIDER_H_ */
--- src/file_sequence_image_provider.cpp
+++ src/file_sequence_image_provider.cpp
@@ -1,31 +1,26 @@
 #include "file_sequence_image_provider.h"
 
 FileSequenceImageProvider::FileSequenceImageProvider(std::string directory) {
-  this->it = std::filesystem::directory_iterator(directory);
+  this->directory = std::filesystem::path(directory);
   this->image_counter_ = 0;
+  this->start_index_ = 1;
+  this->end_index_ = 359; // TODO: Change and get length from number of images in directory.
+  this->image_type_ = ImageFileType::kJpg;
 }
 
-FileSequenceImageProvider::~FileSequenceImageProvider() {}
-
-cv::Mat& FileSequenceImageProvider::GetNextImage() {
-
-  // TODO: Refactor logic since directory_iterator cannot guarantee an order.
-
-  std::filesystem::directory_iterator end_it = std::filesystem::end(it);
-
-  if(it == end_it) {
-    std::filesystem::path no_path("");
-    image_ = cv::imread(no_path);
-    return image_;
+cv::Mat FileSequenceImageProvider::GetNextImage() {
+  if(image_counter_ == end_index_) {
+    return cv::imread(std::filesystem::path(""));
   }
 
-  auto filename = it->path();
-  std::cout << filename << std::endl;
+  std::stringstream image_number;
+  image_number << std::setw(5) << std::setfill('0') << start_index_ + image_counter_;
+  std::stringstream image_full_filename;
+  image_full_filename << directory.c_str() << image_number.str() << "." << ImageFileUtils::ToString(image_type_);
 
-  image_ = cv::imread(filename.string());
-
-  it++;
   image_counter_++;
 
-  return image_;
+  std::cout << "Loading image: " << image_full_filename.str() << std::endl;
+
+  return cv::imread(image_full_filename.str());
 }
