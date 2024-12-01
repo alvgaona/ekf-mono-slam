d74dc7458fae3cd9048a645eee8845ae102abcb2
Alvaro <alvgaona@gmail.com>
2024-12-01 12:25:52 -0300

    squash

--- src/ekf-mono-slam/src/feature/image_feature_prediction.cpp
+++ src/ekf-mono-slam/src/feature/image_feature_prediction.cpp
@@ -3,15 +3,15 @@
 #include "math/ekf_math.h"
 
 ImageFeaturePrediction::ImageFeaturePrediction(const cv::Point& coordinates)
-    : ImageFeature(coordinates) {}
+  : ImageFeature(coordinates) {}
 
 ImageFeaturePrediction ImageFeaturePrediction::from(
-    const Eigen::Vector3d& directionalVector
+  const Eigen::Vector3d& directionalVector
 ) {
   const auto undistorted_image_feature =
-      UndistortedImageFeature::Project(directionalVector);
+    UndistortedImageFeature::Project(directionalVector);
   const auto distorted_feature =
-      EkfMath::distortImageFeature(undistorted_image_feature);
+    EkfMath::distortImageFeature(undistorted_image_feature);
 
   return ImageFeaturePrediction(distorted_feature);
 }
