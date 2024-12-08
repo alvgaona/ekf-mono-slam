e6077155e797be0b9fca4f9cb237118e89adaa32
Alvaro <alvgaona@gmail.com>
2024-12-08 19:30:24 -0300

    feat: declare new parameters

--- src/ekf-mono-slam/src/file_sequence_image_node.cpp
+++ src/ekf-mono-slam/src/file_sequence_image_node.cpp
@@ -1,5 +1,6 @@
 #include "file_sequence_image_node.h"
 
+#include <cstdint>
 #include <functional>
 #include <memory>
 #include <string>
@@ -11,10 +12,18 @@ using namespace std::chrono_literals;
 
 FileSequenceImageNode::FileSequenceImageNode() : Node("file_sequence_image") {
   this->declare_parameter("image_dir", "");
-  auto image_dir = this->get_parameter("image_dir").get_value<std::string>();
+  this->declare_parameter("start_image_index", 1);
+  this->declare_parameter("end_image_index", 350);
 
-  image_provider_ =
-    std::make_unique<FileSequenceImageProvider>(image_dir, 1, 359);
+  auto image_dir = this->get_parameter("image_dir").get_value<std::string>();
+  auto start_image_index =
+    this->get_parameter("start_image_index").get_value<uint16_t>();
+  auto end_image_index =
+    this->get_parameter("end_image_index").get_value<uint16_t>();
+
+  image_provider_ = std::make_unique<FileSequenceImageProvider>(
+    image_dir, start_image_index, end_image_index
+  );
   image_publisher_ = std::make_shared<image_transport::Publisher>(
     image_transport::create_publisher(this, "camera/image")
   );
@@ -24,14 +33,16 @@ FileSequenceImageNode::FileSequenceImageNode() : Node("file_sequence_image") {
 void FileSequenceImageNode::timer_callback() const {
   const cv::Mat image = image_provider_->next();
 
-  cv_bridge::CvImage cv_bridge_image;
-  cv_bridge_image.encoding = sensor_msgs::image_encodings::BGR8;
-  cv_bridge_image.image = image;
+  if (!image.empty()) {
+    cv_bridge::CvImage cv_bridge_image;
+    cv_bridge_image.encoding = sensor_msgs::image_encodings::BGR8;
+    cv_bridge_image.image = image;
 
-  sensor_msgs::msg::Image::SharedPtr image_msg =
-    cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image).toImageMsg();
+    sensor_msgs::msg::Image::SharedPtr image_msg =
+      cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image).toImageMsg();
 
-  image_publisher_->publish(*image_msg);
+    image_publisher_->publish(*image_msg);
+  }
 }
 
 int main(const int argc, char *argv[]) {
