a5e24e2407ff9a8d51f4129775353b1980cae753
Alvaro <alvgaona@gmail.com>
2024-12-16 23:28:33 -0300

    chores: compute more jacobians

--- src/ekf-mono-slam/src/feature/inverse_depth_map_feature.cpp
+++ src/ekf-mono-slam/src/feature/inverse_depth_map_feature.cpp
@@ -54,7 +54,28 @@ Eigen::MatrixXd InverseDepthMapFeature::measurement_jacobian(
   auto dqcw_dqwc = Eigen::Matrix4d::Identity();
   dqcw_dqwc.diagonal() = Eigen::Vector4d(1, -1, -1, -1);
 
-  const auto dhi_dqwc = dhd_dhu * dhu_dhc;  // TODO: complete
+  const auto hw = MapFeature::directional_vector(camera_position);
+
+  const auto qcw = Eigen::Quaterniond(rotation_matrix);
+
+  auto dhc_dqcw = Eigen::MatrixXd(3, 4);
+
+  dhc_dqcw.block(0, 0, 3, 1) =
+    EkfMath::rotation_matrix_derivatives_by_q0(qcw) * hw;
+  dhc_dqcw.block(0, 1, 3, 1) =
+    EkfMath::rotation_matrix_derivatives_by_q1(qcw) * hw;
+  dhc_dqcw.block(0, 2, 3, 1) =
+    EkfMath::rotation_matrix_derivatives_by_q2(qcw) * hw;
+  dhc_dqcw.block(0, 3, 3, 1) =
+    EkfMath::rotation_matrix_derivatives_by_q3(qcw) * hw;
+
+  const auto dhc_dqwc = dhc_dqcw * dqcw_dqwc;
+
+  const auto dhi_dqwc = dhd_dhu * dhu_dhc * dhc_dqwc;
+
+  auto dhi_dxc = Eigen::MatrixXd::Zero(2, 13);
+  dhi_dxc.block(0, 0, 2, 3) = dhi_drwc;
+  dhi_dxc.block(0, 2, 2, 4) = dhi_dqwc;
 
   // TOOD: continue
 }
